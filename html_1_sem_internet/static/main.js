ff77caaa9cb5eadf8763bcbb785b0ac2d193d2eb
thal-ia <thalia.nunes12@hotmai.com>
2024-05-29 16:50:08 -0300

    commit1

---
+++ html_1_sem_internet/static/main.js
@@ -0,0 +1,14 @@
+function changes (){
+    var fundo = document.getElementById('p1')
+
+    if (fundo.className == "class1")
+    {
+        fundo.className = "class2"
+    } 
+    else 
+    {
+        fundo.className = "class1"
+    }
+    
+}
+
